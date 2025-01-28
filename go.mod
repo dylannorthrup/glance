5fc54dcb7d19ec34f1c2ec448e158074aeff6705
Dylan Northrup <northrup@ufl.edu>
2025-01-28 09:53:29 -0500

    Changes to cause glance to reload (and triger page reload) on config file change

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/glanceapp/glance
 go 1.22.5
 
 require (
+	github.com/fsnotify/fsnotify v1.8.0
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
 	github.com/mmcdole/gofeed v1.3.0
 	golang.org/x/text v0.21.0
 	gopkg.in/yaml.v3 v3.0.1
@@ -16,4 +19,5 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 )
--- internal/glance/main.go
+++ internal/glance/main.go
@@ -3,6 +3,14 @@ package glance
 import (
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+var (
+	currentApp *Application
+	done       chan bool
 )
 
 func Main() int {
@@ -13,11 +21,68 @@ func Main() int {
 		return 1
 	}
 
-	configFile, err := os.Open(options.ConfigPath)
+	if options.Intent == CliIntentServe {
+		err := startWatcherAndApp(options.ConfigPath, false)
+		if err != nil {
+			fmt.Println(err)
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func startWatcherAndApp(configPath string, sendReload bool) error {
+	done = make(chan bool)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return fmt.Errorf("failed to create file watcher: %v", err)
+	}
+	defer watcher.Close()
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fmt.Println("config file modified, restarting application...")
+					if currentApp != nil {
+						if err := currentApp.Stop(); err != nil {
+							fmt.Printf("failed to shutdown application: %v\n", err)
+						}
+						time.Sleep(1 * time.Second) // give it enough time to shutdown
+					}
+					startWatcherAndApp(configPath, true)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				fmt.Printf("error watching config file: %v\n", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(configPath)
+	if err != nil {
+		return fmt.Errorf("failed to watch config file: %v", err)
+	}
+
+	restartApplication(configPath, sendReload)
+	<-done
+
+	return nil
+}
+
+func restartApplication(configPath string, sendReload bool) {
+	configFile, err := os.Open(configPath)
 
 	if err != nil {
 		fmt.Printf("failed opening config file: %v\n", err)
-		return 1
+		return
 	}
 
 	config, err := NewConfigFromYml(configFile)
@@ -25,22 +90,32 @@ func Main() int {
 
 	if err != nil {
 		fmt.Printf("failed parsing config file: %v\n", err)
-		return 1
+		return
 	}
 
-	if options.Intent == CliIntentServe {
-		app, err := NewApplication(config)
+	app, err := NewApplication(config)
+	if err != nil {
+		fmt.Printf("failed creating application: %v\n", err)
+		return
+	}
+	currentApp = app
 
-		if err != nil {
-			fmt.Printf("failed creating application: %v\n", err)
-			return 1
-		}
-
-		if err := app.Serve(); err != nil {
-			fmt.Printf("http server error: %v\n", err)
-			return 1
-		}
+	if err != nil {
+		fmt.Printf("failed creating application: %v\n", err)
+		return
 	}
 
-	return 0
+	if err := app.Serve(); err != nil {
+		fmt.Printf("http server error: %v\n", err)
+		return
+	}
+	if sendReload {
+		wsBroadcast <- []byte("reload")
+	}
+
+	if err := app.Serve(); err != nil {
+		fmt.Printf("http server error: %v\n", err)
+	}
+
+	return
 }
